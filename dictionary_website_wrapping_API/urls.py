805a68392cd197a8537f36d41ac1ce160a5fc817
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-21 20:39:35 +0000

    configured API app

--- dictionary_website_wrapping_API/urls.py
+++ dictionary_website_wrapping_API/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dictionary_website.urls',namespace='website')),
+    path('api/',include('wrappingAPI.urls',namespace='api')),
 ]
 
 if settings.DEBUG:
