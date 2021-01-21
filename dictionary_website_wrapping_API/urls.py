e1b136fc0d62719424a4775618e07e60aa43bd00
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-21 20:30:58 +0000

    added base.html and homepage layout

--- dictionary_website_wrapping_API/urls.py
+++ dictionary_website_wrapping_API/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dictionary_website.urls',namespace=website)),
+    path('', include('dictionary_website.urls',namespace='website')),
 ]
 
 if settings.DEBUG:
