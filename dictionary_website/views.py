7fb6ba4d0fe8c5d8884e4711abdd9eab713d26d8
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-22 19:23:49 +0000

    handle post data to view

--- dictionary_website/views.py
+++ dictionary_website/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .helpers import contactAPI
 
 def home(request):
+
+    if request.method =='POST':
+        search_word = request.POST.get('search_word')
+        for i in contactAPI(search_word)['results']:
+            print(i)
+            print()
+            print()
     return render(request,'dictionary_website/home.html')
