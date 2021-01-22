196322cf922906039b6110530803672147e71e82
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-22 22:46:35 +0000

    finished basic functionality

--- dictionary_website/views.py
+++ dictionary_website/views.py
@@ -3,10 +3,24 @@ from .helpers import contactAPI
 
 def home(request):
 
+    context={}
+
     if request.method =='POST':
         search_word = request.POST.get('search_word')
-        for i in contactAPI(search_word)['results']:
-            print(i)
-            print()
-            print()
-    return render(request,'dictionary_website/home.html')
+       
+        search_result=contactAPI(search_word)
+        try:
+        
+            context={
+                'word':search_result['word'],
+                'pronunciation':search_result['pronunciation'],
+                'results':search_result['results'],
+                'method':'POST'
+            }
+        except KeyError:
+            if search_result['success'] == False:
+                context={
+                    'error':search_result['message']
+                }
+
+    return render(request,'dictionary_website/home.html',context=context)
