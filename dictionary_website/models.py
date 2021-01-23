5f7d04a452248e4a97a8f100c940d6226af22f37
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-23 00:10:07 +0000

    add a simple word of the day model

--- dictionary_website/models.py
+++ dictionary_website/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class WordOfTheDay(models.Model):
+    word=models.CharField(max_length=225)
+    definition=models.TextField()
+    timestamp=models.DateTimeField(auto_now=True)
